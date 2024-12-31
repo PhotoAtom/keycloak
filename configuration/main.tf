fbe868f109969455375c99a36fc6dd3156620b26
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2024-12-31 12:48:01 +0530

    ConfigMap Namespace Update

--- configuration/main.tf
+++ configuration/main.tf
@@ -1,6 +1,7 @@
 resource "kubernetes_config_map" "realm_configuration" {
   metadata {
-    name = "photoatom-realm-configuration"
+    name      = "photoatom-realm-configuration"
+    namespace = var.namespace
     labels = {
       app       = "keycloak"
       component = "configmap"
