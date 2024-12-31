65be493bf7d48fd4baeeb12eca3fd16b424c6bb4
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2024-12-31 12:34:57 +0530

    PhotoAtom Realm Configuration

---
+++ configuration/main.tf
@@ -0,0 +1,13 @@
+resource "kubernetes_config_map" "realm_configuration" {
+  metadata {
+    name = "photoatom-realm-configuration"
+    labels = {
+      app       = "keycloak"
+      component = "configmap"
+    }
+  }
+
+  data = {
+    "realm.json" = "${file("${path.module / photoatom.json}")}"
+  }
+}
